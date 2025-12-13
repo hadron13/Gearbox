e7fe10ee2f3e520c7a1ee68e93e7de146292bdc8
hadron13 <carlosbpfonseca@gmail.com>
2025-12-13 11:37:27 -0300

    Fix fluid textures

--- src/main/java/io/github/hadron13/gearbox/register/ModFluids.java
+++ src/main/java/io/github/hadron13/gearbox/register/ModFluids.java
@@ -1,37 +1,65 @@
 package io.github.hadron13.gearbox.register;
 
+import com.simibubi.create.AllFluids;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.infrastructure.config.AllConfigs;
+import com.tterrag.registrate.builders.FluidBuilder;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import io.github.hadron13.gearbox.Gearbox;
+import net.createmod.catnip.theme.Color;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
+import org.joml.Vector3f;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class ModFluids {
     private static final CreateRegistrate REGISTRATE = Gearbox.registrate().setCreativeTab(ModCreativeTabs.MAIN_TAB);
 
+//    public static final FluidEntry<ForgeFlowingFluid.Flowing> PETROLEUM = REGISTRATE
+//            .fluid("petroleum",
+//                    Gearbox.asResource("fluid/petroleum_still"),
+//                    Gearbox.asResource("fluid/petroleum_flow"))
+//            .lang("Petroleum")
+//            .properties(p -> p.viscosity(2000).density(1500))
+//            .fluidProperties(p -> p.levelDecreasePerBlock(3)
+//                            .tickRate(25)
+//                            .slopeFindDistance(3)
+//                            .explosionResistance(100f))
+//            .source(ForgeFlowingFluid.Source::new)
+//            .bucket()
+//            .tag(AllTags.forgeItemTag("buckets/petroleum"))
+//            .build().register();
 
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> PETROLEUM = REGISTRATE
-            .fluid("petroleum",
-                    Gearbox.asResource("fluid/petroleum_still"),
-                    Gearbox.asResource("fluid/petroleum_flow"))
-            .lang("Petroleum")
-            .properties(p -> p.viscosity(2000).density(1500))
-            .fluidProperties(p -> p.levelDecreasePerBlock(3)
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> PETROLEUM =
+            REGISTRATE.standardFluid("petroleum",
+                            SolidRenderedPlaceableFluidType.create(0x444444,
+                                    () -> 1f / 32f ))
+                    .lang("Petroleum")
+                    .properties(b -> b.viscosity(2000)
+                            .density(1400))
+                    .fluidProperties(p -> p.levelDecreasePerBlock(2)
                             .tickRate(25)
                             .slopeFindDistance(3)
                             .explosionResistance(100f))
-            .source(ForgeFlowingFluid.Source::new)
-            .bucket()
-            .tag(AllTags.forgeItemTag("buckets/petroleum"))
-            .build().register();
+                    .source(ForgeFlowingFluid.Source::new)
+                    .block()
+                    .properties(p -> p.mapColor(MapColor.TERRACOTTA_YELLOW))
+                    .build()
+                    .bucket()
+//                    .onRegister(AllFluids::registerFluidDispenseBehavior)
+                    .tag(AllTags.forgeItemTag("buckets/petroleum")) //TODO: remove this
+                    .build()
+                    .register();
 
     public static final FluidEntry<ForgeFlowingFluid.Flowing> RESIN = REGISTRATE
             .fluid("resin",
@@ -99,6 +127,54 @@ public class ModFluids {
                 }
             });
         }
+    }
+
+
+
+    private static class SolidRenderedPlaceableFluidType extends AllFluids.TintedFluidType {
+
+        private Vector3f fogColor;
+        private Supplier<Float> fogDistance;
+
+        public static FluidBuilder.FluidTypeFactory create(int fogColor, Supplier<Float> fogDistance) {
+            return (p, s, f) -> {
+                SolidRenderedPlaceableFluidType fluidType = new SolidRenderedPlaceableFluidType(p, s, f);
+                fluidType.fogColor = new Color(fogColor, false).asVectorF();
+                fluidType.fogDistance = fogDistance;
+                return fluidType;
+            };
+        }
+
+        private SolidRenderedPlaceableFluidType(Properties properties, ResourceLocation stillTexture,
+                                                ResourceLocation flowingTexture) {
+            super(properties, stillTexture, flowingTexture);
+        }
+
+        @Override
+        protected int getTintColor(FluidStack stack) {
+            return NO_TINT;
+        }
+
+        /*
+         * Removing alpha from tint prevents optifine from forcibly applying biome
+         * colors to modded fluids (this workaround only works for fluids in the solid
+         * render layer)
+         */
+        @Override
+        public int getTintColor(FluidState state, BlockAndTintGetter world, BlockPos pos) {
+            return 0x00ffffff;
+        }
+
+        @Override
+        protected Vector3f getCustomFogColor() {
+            return fogColor;
+        }
+
+        @Override
+        protected float getFogDistanceModifier() {
+            return fogDistance.get();
+        }
+
     }
 
     public static void register() {}
